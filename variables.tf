f95163a35ac77cc71d93a43bf4d9cd9497b6708a
sandeept1986 <33664662+sandeept1986@users.noreply.github.com>
2022-04-11 16:16:27 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,4 @@
-variable "region" {
-    default = "us-east-1"
-  description = "This is the cloud hosting region where your webapp will be deployed."
-}
-variable "prefix" {
-    default = "dev"
-  description = "This is the environment your webapp will be prefixed with. dev, qa, or prod"
-  
-}
-variable "name" {
-      default = "sampleapp"
-   description = "Your name to attach to the webapp address"
+variable "region" {}
+variable "prefix" {}
+variable "name" {}
       
-}
